2cd842c636f53078020358e5099ab1585a606e80
Jeff Wagner <j5wagner@nif1.crbs.ucsd.edu>
2016-09-14 21:59:26 -0700

    Made directory naming follow cookiecutter variables

--- {{cookiecutter.package_name}}/test_data/test.sh
+++ {{cookiecutter.package_name}}/test_data/test.sh
@@ -12,14 +12,14 @@ mkdir 1-get_challenge_data
 getchallengedata.py --unpackdir 1-get_challenge_data --localdata celpp_weekXX_XXXX.tar.gz 
 
 mkdir 2-protein_prep
-python ../cookiecutter-custom-celpp-contestant/custom_celpp_contestant_protein_prep.py --challengedata 1-get_challenge_data/celpp_weekXX_XXXX --prepdir 2-protein_prep
+python ../{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_protein_prep.py --challengedata 1-get_challenge_data/celpp_weekXX_XXXX --prepdir 2-protein_prep
 
 
 mkdir 3-ligand_prep
-python ../cookiecutter-custom-celpp-contestant/custom_celpp_contestant_ligand_prep.py --challengedata 1-get_challenge_data/celpp_weekXX_XXXX --prepdir 3-ligand_prep
+python ../{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_ligand_prep.py --challengedata 1-get_challenge_data/celpp_weekXX_XXXX --prepdir 3-ligand_prep
 
 mkdir 4-docking
-python ../cookiecutter-custom-celpp-contestant/custom_celpp_contestant_dock.py --protsciprepdir 2-protein_prep --ligsciprepdir 3-ligand_prep --outdir  4-docking
+python ../{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_dock.py --protsciprepdir 2-protein_prep --ligsciprepdir 3-ligand_prep --outdir  4-docking
 
 mkdir 5-pack_docking_results
 packdockingresults.py --dockdir 4-docking --packdir 5-pack_docking_results

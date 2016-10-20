703c153959df20eefecf42e216b4c40da56d5661
j-wags <jwagnerjpl@gmail.com>
2016-10-19 18:02:19 -0700

    Update {{cookiecutter.program_name}}_dock.py

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_dock.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_dock.py
@@ -13,7 +13,7 @@ class {{cookiecutter.dock_class_name}}(Dock):
     Dock.SCI_PREPPED_PROT_SUFFIX = '_prepared.pdb'
 
 
-    def lig_technical_prep(self, sci_prepped_lig, targ_info_dict = {}):
+    def lig_tech_prep(self, sci_prepped_lig, targ_info_dict = {}):
         """
         'Technical preparation' is the step immediate preceding
         docking. During this step, you may perform any file
@@ -26,8 +26,8 @@ class {{cookiecutter.dock_class_name}}(Dock):
         returns the input string in a list (ie. [sci_prepped_lig]) 
         """
         return super({{cookiecutter.dock_class_name}},
-                     self).lig_technical_prep(sci_prepped_lig,
-                                              targ_info_dict = targ_info_dict)
+                     self).lig_tech_prep(sci_prepped_lig,
+                                         targ_info_dict = targ_info_dict)
 
     def receptor_technical_prep(self, sci_prepped_receptor, pocket_center, targ_info_dict = {}):
         """

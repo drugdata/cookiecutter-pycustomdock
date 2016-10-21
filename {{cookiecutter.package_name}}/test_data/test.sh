f2ce8f8fb2ff58a9b029894fa03ac5a00f637618
Jeff Wagner <j5wagner@nif1.crbs.ucsd.edu>
2016-10-21 00:15:05 -0700

    formatting changes

--- {{cookiecutter.package_name}}/test_data/test.sh
+++ {{cookiecutter.package_name}}/test_data/test.sh
@@ -11,11 +11,11 @@ mkdir 1-get_challenge_data
 getchallengedata.py --unpackdir 1-get_challenge_data --localdata celpp_weekXX_XXXX.tar.gz 
 
 mkdir 2-protein_prep
-python ../{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_protein_prep.py --challengedata 1-get_challenge_data/celpp_weekXX_XXXX --prepdir 2-protein_prep
+python ../{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_protein_prep.py --challengedata 1-get_challenge_data --prepdir 2-protein_prep
 
 
 mkdir 3-ligand_prep
-python ../{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_ligand_prep.py --challengedata 1-get_challenge_data/celpp_weekXX_XXXX --prepdir 3-ligand_prep
+python ../{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_ligand_prep.py --challengedata 1-get_challenge_data --prepdir 3-ligand_prep
 
 mkdir 4-docking
 python ../{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_dock.py --protsciprepdir 2-protein_prep --ligsciprepdir 3-ligand_prep --outdir  4-docking

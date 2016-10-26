f69d5c6dd018a56b2ad89aaadbada951cf7e832d
j-wags <jwagnerjpl@gmail.com>
2016-10-25 19:47:52 -0700

    Update full_week_run.sh

--- {{cookiecutter.package_name}}/full_week_run/full_week_run.sh
+++ {{cookiecutter.package_name}}/full_week_run/full_week_run.sh
@@ -1,4 +1,4 @@
-# Clear previous test results so we don't get mixed up
+# Clear previous results so we don't get mixed up
 rm -r 1-get_challenge_data
 rm -r 2-protein_prep
 rm -r 3-ligand_prep
@@ -11,15 +11,13 @@ mkdir 1-get_challenge_data
 getchallengedata.py --unpackdir 1-get_challenge_data -f ../ftp_config
 
 mkdir 2-protein_prep
-python ../jeffs_fred_implementation/jeffs_fred_implementation_protein_prep.py --challengedata 1-get_challenge_data/ --prepdir 2-protein_prep
-
+python ../{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_protein_prep.py --challengedata 1-get_challenge_data/ --prepdir 2-protein_prep
 
 mkdir 3-ligand_prep
-python ../jeffs_fred_implementation/jeffs_fred_implementation_ligand_prep.py --challengedata 1-get_challenge_data/ --prepdir 3-ligand_prep
+python ../{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_ligand_prep.py --challengedata 1-get_challenge_data/ --prepdir 3-ligand_prep
 
 mkdir 4-docking
-python ../jeffs_fred_implementation/jeffs_fred_implementation_dock.py --protsciprepdir 2-protein_prep --ligsciprepdir 3-ligand_prep --outdir  4-docking
+python ../{{cookiecutter.package_name}}/{{cookiecutter.program_name}}_dock.py --protsciprepdir 2-protein_prep --ligsciprepdir 3-ligand_prep --outdir  4-docking
 
 mkdir 5-pack_docking_results
 packdockingresults.py --dockdir 4-docking --packdir 5-pack_docking_results -f ../ftp_config --challengedata 1-get_challenge_data/
-
